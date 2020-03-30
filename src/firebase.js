f78429c7246f0328cd1708daa1db38b37133856c
fooheeyuan <61861689+fooheeyuan@users.noreply.github.com>
2020-03-31 04:12:18 +0800

    add CallAPI

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,16 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const firebaseConfig = {
-    //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
+  apiKey: "AIzaSyDSbkOuoSUhQpKp_-Zh02UKfHMocI10CdE",
+  authDomain: "fir-tutorial-ae30a.firebaseapp.com",
+  databaseURL: "https://fir-tutorial-ae30a.firebaseio.com",
+  projectId: "fir-tutorial-ae30a",
+  storageBucket: "fir-tutorial-ae30a.appspot.com",
+  messagingSenderId: "209721389739",
+  appId: "1:209721389739:web:c1941580e6d46d63dafdee",
+  measurementId: "G-76855EEF96"
+};
 
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.firestore();
-  export default database;
+firebase.initializeApp(firebaseConfig);
+var database = firebase.firestore();
+export default database;
